b7a5b14135dc61044e83a3feb331db83b27818ee
Jeans, Rayland <Rayland.Jeans@pearson.com>
2014-02-03 11:03:10 -0700

    Update metrics

--- reporter.go
+++ reporter.go
@@ -2,88 +2,39 @@ package main
 
 import (
 	"fmt"
-	"github.com/bmizerany/perks/quantile"
 	"strings"
 	"time"
-	"math"
 )
 
 func report(results []Result, name string) {
-	quants := quantile.NewTargeted(0.95, 0.99)
+	calc := NewMetrics(results)
+	calc.Sort()
 
-	total := float64(len(results))
-	totalRtt := time.Duration(0)
-	min := time.Hour
-	max := time.Duration(0)
-
-	for _, result := range results {
-		quants.Insert(float64(result.Duration))
-		totalRtt += result.Duration
-
-		if result.Duration > max {
-			max = result.Duration
-		}
-
-		if result.Duration < min {
-			min = result.Duration
-		}
-	}
-
-	mean := float64(totalRtt)/total
-	stdDev := getDistributions(total, mean, results)
-
-	_75 := quants.Query(0.75)
-	_25 := quants.Query(0.25)
+	_25  := calc.GetPercentile(0.25)
+	_75  := calc.GetPercentile(0.75)
+	_95  := calc.GetPercentile(0.95)
+	_99  := calc.GetPercentile(0.99)
+	_999 := calc.GetPercentile(0.999)
 
 	fmt.Println(strings.Repeat("=", 30))
 	fmt.Println("Results -", name)
 	fmt.Println(strings.Repeat("-", 30))
-	fmt.Println("Requests:", total)
+	fmt.Println("Requests:", calc.Total)
 
 	fmt.Println("Latencies:")
-	fmt.Printf("\tTotal:\t%s\t\n\n", totalRtt)
+	fmt.Printf("\tTotal:\t%s\t\n\n", calc.TotalRtt)
 
-	fmt.Printf("\t0.25:\t%s\n", time.Duration(_25))
-	fmt.Printf("\t0.75:\t%s\n", time.Duration(_75))
-	fmt.Printf("\t0.95:\t%s\n", time.Duration(quants.Query(0.95)))
-	fmt.Printf("\t0.99:\t%s\n", time.Duration(quants.Query(0.99)))
-	fmt.Printf("\t0.999:\t%s\n\n", time.Duration(quants.Query(0.999)))
+	fmt.Printf("\t0.25:\t%s\n", _25)
+	fmt.Printf("\t0.75:\t%s\n", _75)
+	fmt.Printf("\t0.95:\t%s\n", _95)
+	fmt.Printf("\t0.99:\t%s\n", _99)
+	fmt.Printf("\t0.999:\t%s\n\n", _999)
 
-	fmt.Printf("\tmean:\t%s\n", time.Duration(mean))
-	fmt.Printf("\tstd:\t%s\n", time.Duration(stdDev))
-	fmt.Printf("\tmin:\t%s\n", min)
-	fmt.Printf("\tmax:\t%s\n", max)
+	fmt.Printf("\tmean:\t%s\n", time.Duration(calc.Mean))
+	fmt.Printf("\tstd:\t%s\n", time.Duration(calc.StdDev()))
+	fmt.Printf("\tmin:\t%s\n", calc.Min)
+	fmt.Printf("\tmax:\t%s\n", calc.Max)
 	fmt.Printf("\tiqr:\t%s\n", time.Duration(_75 - _25) )
 
-	fmt.Println("\nPercentiles:")
-	calc := NewCalculator(results, totalRtt.Nanoseconds())
-	calc.Sort()
-
-	v1_99  := calc.GetPercentile1(0.99)
-	v1_999 := calc.GetPercentile1(0.999)
-	v2_99  := calc.GetPercentile2(0.99)
-	v2_999 := calc.GetPercentile2(0.999)
-
-	fmt.Println("version1:")
-	fmt.Printf("\t0.99:\t%s\n", v1_99)
-	fmt.Printf("\t0.999:\t%s\n", v1_999)		
-	fmt.Println("\nversion2:")
-	fmt.Printf("\t0.99:\t%s\n", v2_99)
-	fmt.Printf("\t0.999:\t%s\n", v2_999)		
-
 	fmt.Println(strings.Repeat("=", 30))
-}
-
-func getDistributions(numResults float64, mean float64, results []Result) float64 {
-	var diffs float64
-	m := float64(time.Duration(mean).Nanoseconds())
-
-	for _, result := range results {
-		diffs += math.Pow(float64(result.Duration.Nanoseconds()) - m, 2)
-	}
-
-	variance := diffs / numResults
-	stdDev := math.Sqrt(variance)
-
-	return stdDev
 }
